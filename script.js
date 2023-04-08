6513ff6977066e1511897abb45a45171d5183914
Fornikorte <raviel282@gmail.com>
2023-04-07 22:49:40 -0400

    Se agrega imagen futura para logo

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 $(".futuro").on("click",function(){
     alert("proximamente");
 });
+
+$("#formulario-contacto").submit(function(e){
+    e.preventDefault();
+});
+
+$(document).ready(function() { $("#formulario-contacto").validate(
+    {
+        messages:{
+            nombre:{
+                required:"*Campo obligatorio"
+            },
+            correo:{
+                required:"*Campo obligatorio"
+            }
+        }
+    }
+); });
